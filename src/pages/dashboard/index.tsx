d218feb0cf3b85219751ee605fa703797eba01d2
quanvo0213 <quan.vo@katalon.com>
2023-12-03 19:46:02 +0700

    remove unuse code

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -15,84 +15,6 @@ import { BarChartExample3 } from "./chartView";
 import { DateRangePickerCustom } from "../../components/dateRangePicker";
 import { TableExample } from "./table";
 
-
-const data = {
-  "type": "Execution",
-  "conditions": [
-    {
-      "key": "startTime",
-      "operator": ">=",
-      "value": "2023-11-27T00:00:00Z"
-    },
-    {
-      "key": "startTime",
-      "operator": "<",
-      "value": "2023-12-04T00:00:00Z"
-    },
-    {
-      "key": "Project.id",
-      "operator": "=",
-      "value": "3071"
-    }
-  ],
-  "functions": [
-    {
-      "key": "total",
-      "function": "count_distinct",
-      "parameters": [
-        "id"
-      ]
-    },
-    {
-      "key": "time",
-      "function": "group_by_day",
-      "parameters": [
-        "startTime"
-      ]
-    }
-  ],
-  "pagination": {
-    "page": 0,
-    "size": 300,
-    "sorts": [
-      "time, desc"
-    ]
-  },
-  "groupBys": [
-    "status"
-  ]
-};
-
-const dataTable = {
-  "type": "Execution",
-  "conditions": [
-    {
-      "key": "startTime",
-      "operator": ">=",
-      "value": "2023-11-27T00:00:00Z"
-    },
-    {
-      "key": "startTime",
-      "operator": "<",
-      "value": "2023-12-04T00:00:00Z"
-    },
-    {
-      "key": "Project.id",
-      "operator": "=",
-      "value": "3071"
-    }
-  ],
-  "functions": [],
-  "pagination": {
-    "page": 0,
-    "size": 30,
-    "sorts": [
-      "order,desc"
-    ]
-  },
-  "groupBys": []
-};
-
 export const DashboardPage: React.FC = () => {
 
   const buildChartQuery = (from?: Date, to?: Date) => ({
@@ -172,11 +94,14 @@ export const DashboardPage: React.FC = () => {
     "groupBys": []
   });
 
-  const [executionsGroupByQuery, setExecutionsGroupByQuery] = useState<any>(data);
-  const [executionsQuery, setExecutionsQuery] = useState<any>(dataTable);
+  const defaultStartDate = new Date(2023, 10, 28);
+  const defaultEndDate = new Date();
+
+  const [executionsGroupByQuery, setExecutionsGroupByQuery] = useState<any>(buildChartQuery(defaultStartDate, defaultEndDate));
+  const [executionsQuery, setExecutionsQuery] = useState<any>(buildTableQuery(defaultStartDate, defaultEndDate));
   const [date, setDate] = useState<DateRangePickerValue>({
-    from: new Date(2023, 10, 28),
-    to: new Date(),
+    from: defaultStartDate,
+    to: defaultEndDate,
   });
 
   const setData = (v: any) => {
